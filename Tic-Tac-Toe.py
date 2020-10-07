586e2a8fda8acf41faf59dc70e51093d0496649c
Ayushithakre12 <66862454+Ayushithakre12@users.noreply.github.com>
2020-10-07 18:03:29 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import tkinter.messagebox
 tk = Tk()
-tk.title("Tic Tac Toe")
+tk.title("Tic -Tac -Toe")
 
 pa = StringVar()
 playerb = StringVar()
@@ -46,7 +46,7 @@ def btnClick(buttons):
         checkForWin()
         flag += 1
     else:
-        tkinter.messagebox.showinfo("Tic-Tac-Toe", "Button already Clicked!")
+        tkinter.messagebox.showinfo("Tic-Tac-Toe", "opps...!Button already Clicked! please,click another button.")
 
 def checkForWin():
     if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or
@@ -79,38 +79,38 @@ def checkForWin():
 
 buttons = StringVar()
 
-label = Label( tk, text="Player 1:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
+label = Label( tk, text="Player 1:", font='Times 20 bold', bg='blue', fg='black', height=1, width=8)
 label.grid(row=1, column=0)
 
 
-label = Label( tk, text="Player 2:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
+label = Label( tk, text="Player 2:", font='Times 20 bold', bg='pink', fg='black', height=1, width=8)
 label.grid(row=2, column=0)
 
-button1 = Button(tk, text=" ", font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button1))
+button1 = Button(tk, text=" ", font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button1))
 button1.grid(row=3, column=0)
 
-button2 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button2))
+button2 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button2))
 button2.grid(row=3, column=1)
 
-button3 = Button(tk, text=' ',font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button3))
+button3 = Button(tk, text=' ',font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button3))
 button3.grid(row=3, column=2)
 
-button4 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button4))
+button4 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button4))
 button4.grid(row=4, column=0)
 
-button5 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button5))
+button5 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button5))
 button5.grid(row=4, column=1)
 
-button6 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button6))
+button6 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button6))
 button6.grid(row=4, column=2)
 
-button7 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button7))
+button7 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button7))
 button7.grid(row=5, column=0)
 
-button8 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button8))
+button8 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button8))
 button8.grid(row=5, column=1)
 
-button9 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button9))
+button9 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='blue', height=4, width=8, command=lambda: btnClick(button9))
 button9.grid(row=5, column=2)
 
 tk.mainloop()

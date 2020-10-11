69bc4ae1d5f62c5e45145a8b72fce206cee72e3a
Rishit Khandelwal <65018379+rishit-khandelwal@users.noreply.github.com>
2020-10-11 13:04:18 +0530

    These files didn't get commited

--- Python/merge_sort.py
+++ Python/merge_sort.py
@@ -1,47 +1,34 @@
-# Python program for implementation of MergeSort 
-def mergeSort(arr): 
-    if len(arr) >1: 
-        mid = len(arr)//2 # Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
-  
-        mergeSort(L) # Sorting the first half 
-        mergeSort(R) # Sorting the second half 
-  
-        i = j = k = 0
-          
-        # Copy data to temp arrays L[] and R[] 
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
-                arr[k] = L[i] 
-                i+= 1
-            else: 
-                arr[k] = R[j] 
-                j+= 1
-            k+= 1
-          
-        # Checking if any element was left 
-        while i < len(L): 
-            arr[k] = L[i] 
-            i+= 1
-            k+= 1
-          
-        while j < len(R): 
-            arr[k] = R[j] 
-            j+= 1
-            k+= 1
-  
-# Code to print the list 
-def printList(arr): 
-    for i in range(len(arr)):         
-        print(arr[i], end =" ") 
-    print() 
-  
-# driver code to test the above code 
-if __name__ == '__main__': 
-    arr = [12, 11, 13, 5, 6, 7]  
-    print ("Given array is", end ="\n")  
-    printList(arr) 
-    mergeSort(arr) 
-    print("Sorted array is: ", end ="\n") 
-    printList(arr) 
+def merge_sort(unsorted_list):
+    if len(unsorted_list) <= 1:
+        return unsorted_list
+		
+	# Finding the middle point and partitioning the array into two halves
+    middle = len(unsorted_list) // 2
+    left = unsorted_list[:middle]
+    right = unsorted_list[middle:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return list(merge(left, right))
+
+#Merging the sorted halves
+def merge(left,right):
+
+    res = []
+    while len(left) != 0 and len(right) != 0:
+        if left[0] < right[0]:
+            res.append(left[0])
+            left.remove(left[0])
+        else:
+            res.append(right[0])
+            right.remove(right[0])
+    if len(left) == 0:
+        res = res + right
+    else:
+        res = res + left
+    return res
+
+input_list = list(map(int,input("Enter unsorted input list: ").split()))
+
+print("Unsorted Input: ", input_list)
+print("Sorted Output: ", merge_sort(input_list))

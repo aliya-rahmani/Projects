2aa6b0dcf7949cf136cbf9501afa6324bcd494a5
Parjanya HK <61843804+parjanyahk@users.noreply.github.com>
2020-10-13 21:49:41 +0530

    Update rockPaperScissorGameMain.py

--- Python/rockPaperScissorGame/rockPaperScissorGameMain.py
+++ Python/rockPaperScissorGame/rockPaperScissorGameMain.py
@@ -1,64 +1,61 @@
 # A simple Rock Paper Scissor Game written in python
 # Contributed by Parjanya HK
 
-import random 
+import random
+print("The Rules of Rock paper scissor game will be follows: \n"
++"Rock vs paper --> paper wins \n"
++"Rock vs scissor --> Rock wins \n"
++"paper vs scissor --> scissor wins \n")
 
-while True: 
-	print("Enter choice \n 1. Rock \n 2. paper \n 3. scissor \n") 
+while True:
+print("Now please enter your choice no. \n 1. Rock \n 2. paper \n 3. scissor \n")
+ch = int(input("Now Your turn: "))
+while ch> 3 or ch< 1:
+   ch = int(input("Enter your valid input here: "))
+if ch == 1:
+   choice_name = 'Rock'
+elifch == 2:
+   choice_name = 'paper'
+else:
+   choice_name = 'scissor'
+
+   print("Your choice is: " + choice_name)
+print("\nNow its computer turn to initiate.......")
+
+comp_choice = random.randint(1, 3)
+
+while comp_choice == ch:
+comp_choice = random.randint(1, 3)
+
+if comp_choice == 1:
+   comp_choice_name = 'Rock'
+elifcomp_choice == 2:
+   comp_choice_name = 'paper'
+else:
+   comp_choice_name = 'scissor'
+   print("So computer choice is: " + comp_choice_name)
 	
-	choice = int(input("User turn: ")) 
+print(choice_name + " V/s " + comp_choice_name)
+   # condition for winning the game
+if((ch == 1 and comp_choice == 2) or
+   (ch == 2 and comp_choice ==1 )):
+print("paper wins => ", end = "")
+   final_result = "paper"
+elif((ch == 1 and comp_choice == 3) or
+   (ch == 3 and comp_choice == 1)):
+print("Rock wins =>", end = "")
+   final_result = "Rock"
+else:
+   print("scissor wins =>", end = "")
+   final_result = "scissor"
 
-	while choice > 3 or choice < 1: 
-		choice = int(input("enter valid input: ")) 
-		
-	if choice == 1: 
-		choice_name = 'Rock'
-	elif choice == 2: 
-		choice_name = 'paper'
-	else: 
-		choice_name = 'scissor'
-		
-	print("user choice is: " + choice_name) 
-	print("\nNow its computer turn.......") 
+if final_result == choice_name:
+   print("<== You are the winner ==>")
+else:
+   print("<== Computer wins ==>")
+      print("Do you want to play again? (Y/N)")
+      ans = input()
 
-	comp_choice = random.randint(1, 3) 
-	
-	while comp_choice == choice: 
-		comp_choice = random.randint(1, 3) 
-
-	if comp_choice == 1: 
-		comp_choice_name = 'Rock'
-	elif comp_choice == 2: 
-		comp_choice_name = 'paper'
-	else: 
-		comp_choice_name = 'scissor'
-		
-	print("Computer choice is: " + comp_choice_name) 
-
-	print(choice_name + " V/s " + comp_choice_name) 
-
-	if((choice == 1 and comp_choice == 2) or
-	(choice == 2 and comp_choice ==1 )): 
-		print("paper wins => ", end = "") 
-		result = "paper"
-		
-	elif((choice == 1 and comp_choice == 3) or
-		(choice == 3 and comp_choice == 1)): 
-		print("Rock wins =>", end = "") 
-		result = "Rock"
-	else: 
-		print("scissor wins =>", end = "") 
-		result = "scissor"
-
-	if result == choice_name: 
-		print("<== User wins ==>") 
-	else: 
-		print("<== Computer wins ==>") 
-		
-	print("Do you want to play again? (Y/N)") 
-	ans = input() 
-
-	if ans == 'n' or ans == 'N': 
-		break
-
-print("\nThanks for playing") 
+if ans == 'n' or ans == 'N':
+   break
+print("\nThanks for Playing!")

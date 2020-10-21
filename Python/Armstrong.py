597791c6188836ff56b7953ea898a857c54bd8c3
janvi16 <60323679+janvi16@users.noreply.github.com>
2020-10-22 00:01:02 +0530

    Create Armstrong.py

---
+++ Python/Armstrong.py
@@ -0,0 +1,17 @@
+# Python program to check if the number is an Armstrong number or not
+
+# take input from the user
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")

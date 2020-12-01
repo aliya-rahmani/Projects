72d7036434d67493fa2ce09919a069cd750f1250
afred-khan <72245672+afred-khan@users.noreply.github.com>
2020-12-01 11:19:35 +0530

    Revert "Mul.py"

--- Operation/Mul.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("Enter the first number: "))
-#input value for variable num1
-num2=int(input("Enter the second number: "))
-#input value for variable num2
-mul=num1*num2;
-#perform multiplication operation
-print("the product of given numbers is: ",mul)
-#display the product

5b92dff68e6fd5d7fca47cf0450a9d426e399e5b
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2020-12-16 10:14:49 +0530

    Revert "Debt calculator in python"

--- Python/debt_calculator.py
+++
@@ -1,9 +0,0 @@
-def debt_calc(emprestimo, taxa_juros_mensal, pagamento_mensal):
-    #Calculate how much time it would take to pay a debt
-    #Returns time in months
-    divida = emprestimo
-    t=0
-    while(divida > 0):
-        t+=1
-        divida -= pagamento_mensal - divida*taxa_juros_mensal
-    return t
